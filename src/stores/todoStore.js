8493dad8102b5bd6260027cfb4bdd8aef4462db7
nafimbrk <nafimubarok2005@gmail.com>
2024-12-29 13:58:15 +0700

    To-Do-List Pinia

--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -1,13 +1,61 @@
 import { defineStore } from "pinia";
 
 export const useTodoStore = defineStore("todo", {
-  state: () => ({ count: 5, name: "Eduardo" }),
+  state: () => ({
+    // count: 5, name: "Eduardo"
+
+    todoList: [
+      { name: "Belajar Html", isDone: false },
+      { name: "Belajar Css", isDone: false },
+      { name: "Belajar Javascript", isDone: false },
+      { name: "Belajar Php", isDone: false },
+    ],
+  }),
   getters: {
-    doubleCount: (state) => state.count * 2,
+    // doubleCount: (state) => state.count * 2,
+
+    showAll(state) {
+      return state.todoList;
+    },
+    doneOnly(state) {
+      return state.todoList.filter((item) => item.isDone);
+    },
+    undoneOnly(state) {
+      return state.todoList.filter((item) => !item.isDone);
+    },
   },
   actions: {
-    increment() {
-      this.count++;
+    // increment() {
+    //   this.count++;
+    // },
+
+    setAsDone(name) {
+      // console.log(index)
+      // console.log(this.todoList[index].name)
+      // this.todoList[index].isDone = true;
+
+      // console.log(name);
+      this.todoList.find(item => item.name == name).isDone = true
+    },
+    setAsUndone(name) {
+      // this.todoList[index].isDone = false;
+
+      // console.log(name);
+      this.todoList.find(item => item.name == name).isDone = false
+    },
+    addTodo(data) {
+      // console.log(data)
+
+      let exists = this.todoList.filter((item) => item.name == data).length;
+
+      // console.log(exists)
+
+      if (exists) {
+        alert("new todo is existed in data");
+        return;
+      }
+
+      this.todoList.push({ name: data, isDone: false });
     },
   },
 });
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -5,10 +5,106 @@
     <br>
     {{ todoStore.doubleCount }} -->
 
-    {{ todoStore.count }}
-    <br>
-    <button @click="todoStore.increment">Add</button>
+    <!-- {{ todoStore.count }} -->
+    <!-- <br>
+    <button @click="todoStore.increment">Add</button> -->
+
+    <form action="" @submit.prevent="todoStore.addTodo(newTodo)">
+        <input type="text" name="" id="" v-model="newTodo" placeholder="add new todo...">
+        <input type="submit" name="" id="" value="Add">
+    </form>
+
+    <div style="width: 500px; padding-inline-start: 40px;">
+        <h3 style="text-align: center;">My Todo List</h3>
+    </div>
+
+    <div class="getters-button">
+        <button @click="show = 'all'">Show All</button>
+        <button @click="show = 'done only'">Done Only</button>
+        <button @click=" show = 'undone only'">Undone Only</button>
+    </div>
+
+    <!-- {{ todoStore.todoList }} -->
+    <div v-if=" show == 'all' ">
+        <ul>
+            <li v-for="list in  todoStore.todoList ">
+                <span>
+                    {{ list.name }}
+                </span>
+                <span>
+                    <button v-if=" !list.isDone " @click="todoStore.setAsDone(list.name)">set as done</button>
+                    <button v-if=" list.isDone " @click="todoStore.setAsUndone(list.name)">set as undone</button>
+                </span>
+            </li>
+        </ul>
+    </div>
+
+    <div v-if=" show == 'done only' ">
+        <ul>
+            <li v-for="list in  todoStore.doneOnly ">
+                <span>
+                    {{ list.name }}
+                </span>
+                <span>
+                    <button v-if=" !list.isDone " @click="todoStore.setAsDone(list.name)">set as done</button>
+                    <button v-if=" list.isDone " @click="todoStore.setAsUndone(list.name)">set as undone</button>
+                </span>
+            </li>
+        </ul>
+    </div>
+
+    <div v-if=" show == 'undone only' ">
+        <ul>
+            <li v-for="list in  todoStore.undoneOnly ">
+                <span>
+                    {{ list.name }}
+                </span>
+                <span>
+                    <button v-if=" !list.isDone " @click="todoStore.setAsDone(list.name)">set as done</button>
+                    <button v-if=" list.isDone " @click="todoStore.setAsUndone(list.name)">set as undone</button>
+                </span>
+            </li>
+        </ul>
+    </div>
 </template>
+
+<style scoped>
+form {
+    width: 500px;
+    padding-inline-start: 40px;
+    margin-bottom: 40px;
+}
+
+form input {
+    padding: 10px;
+}
+
+form input:first-child {
+    width: 80%;
+    margin: 0px 10px;
+}
+
+ul {
+    list-style: none;
+    width: 500px;
+}
+
+li {
+    border: solid 1px;
+    margin: 10px;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.getters-button {
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+    width: 500px;
+    padding-inline-start: 40px;
+}
+</style>
 
 <script>
 import { useTodoStore } from '../stores/todoStore'
@@ -18,6 +114,12 @@ export default {
         const todoStore = useTodoStore()
 
         return { todoStore }
+    },
+    data() {
+        return {
+            newTodo: '',
+            show: 'all'
+        }
     }
 }
 </script>
